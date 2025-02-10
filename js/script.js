3802a371221635c5ba0d5656789bddb9b6aad6af
Giorgia Savioli <savioligiorgia@icloud.com>
2025-02-10 17:02:24 +0100

    commit card homepage

--- js/script.js
+++ js/script.js
@@ -42,13 +42,12 @@
 
 //                 document.querySelector(".container").innerHTML = products;
 // });
-
-
-
 fetch('https://fakestoreapi.com/products')
     .then(res => res.json())
     .then(prods => {
-        const products = prods.map(prod => {
+        const randomProducts = prods.sort(() => Math.random() - 0.5).slice(0, 4);
+
+        const products = randomProducts.map(prod => {
             return `
                 <div class="card">
                 <i class="fas fa-heart heart-icon"></i>
@@ -66,5 +65,35 @@ fetch('https://fakestoreapi.com/products')
                 `
                 }).join('')
 
-               document.querySelector(".containerTutti").innerHTML = products;
+               document.querySelector(".container-promozioni").innerHTML = products;
  });
+
+
+ fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(prods => {
+        const randomProducts = prods.sort(() => Math.random() - 0.5).slice(0, 4);
+
+        const products = randomProducts.map(prod => {
+            return `
+                <div class="card">
+                <i class="fas fa-heart heart-icon"></i>
+                    <a href="pag di riferimento per prodotto">
+                    </a>
+                    <img class="immagine" src="${prod.image}" alt="Immagine prodotto">
+                    <h3 class="titolo">${prod.title}</h3>
+                    <div class="prezzo-container">
+                        <p class="prezzo">$${prod.price}</p>
+                           <i class="fas fa-shopping-cart cart-icon"></i>
+                        <a href="pag di riferimento per prodotto">
+                        </a>
+                    </div>
+                </div>
+                `
+                }).join('')
+
+               document.querySelector(".container-disponibili").innerHTML = products;
+ });
+
+
+
