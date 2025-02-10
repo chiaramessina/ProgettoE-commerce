86fa6e43f0519c02e39db1f41f6ebc50fa526321
David95062 <zannidavide44@gmail.com>
2025-02-10 16:07:34 +0100

    modifiche preferiti e carrello,

---
+++ preferiti.js
@@ -0,0 +1,8 @@
+function showPreferiti() {
+    document.getElementById("preferitiDiv").style.display="block";
+}
+function closePreferiti() {
+    document.getElementById("preferitiDiv").style.display="none";
+}
+
+
